9a14c8d7df38cb13c9ff361103f1d81aff86ec13
chstegma <chstegma@3DLABOR2.in.fhkn.de>
2013-10-11 16:48:54 +0200

    testcommit ohne geaddet zu haben

--- testproject1/src/testproject1/Test1.java
+++ testproject1/src/testproject1/Test1.java
@@ -4,7 +4,9 @@ public class Test1 {
 
 	public static void main(String[] args) {
 		System.out.println("Hello World!");
-
+		
+		//neue Zeile 
+		System.out.println("2. testzeile hinzugefügt");
 	}
 
 }
