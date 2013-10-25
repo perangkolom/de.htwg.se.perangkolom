99ab899c19766667412c9118f82eee26e54bf640
chstegma <chstegma@3DLABOR2.in.fhkn.de>
2013-10-25 15:35:54 +0200

    testpush

---
+++ testproject1/game_StructureElements/Border.java
@@ -0,0 +1,18 @@
+package game_StructureElements;
+
+public class Border {
+
+	
+	private boolean borderFilled;
+	private boolean outerBorder;
+	
+	
+	public boolean isOuterBorder() {
+		return outerBorder;
+	}
+	
+	public boolean isBorderFilled() {
+		return borderFilled;
+	}
+	
+}
