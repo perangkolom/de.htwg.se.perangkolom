54a8b71a364e73473ac705801d68de1fd01eb543
chstegma <chstegma@3DLABOR2.in.fhkn.de>
2013-10-25 15:32:50 +0200

    init

---
+++ PerangKolom/src/game_StructureElements/Border.java
@@ -0,0 +1,29 @@
+package game_StructureElements;
+
+public class Border {
+
+	
+	private boolean borderFilled;
+	private boolean outerBorder;
+	
+	
+	public Border(boolean borderFilled, boolean outerBorder) {
+		this.borderFilled = borderFilled;
+		this.outerBorder = outerBorder;
+	}
+	
+	public boolean isBorderFilled() {
+		return borderFilled;
+	}
+	public void setBorderFilled(boolean borderFilled) {
+		this.borderFilled = borderFilled;
+	}
+	public boolean isOuterBorder() {
+		return outerBorder;
+	}
+	public void setOuterBorder(boolean outerBorder) {
+		this.outerBorder = outerBorder;
+	}
+
+	
+}
