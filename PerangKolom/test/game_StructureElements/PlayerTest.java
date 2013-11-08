c276aad871b8ffc9bdedddf81b76ea5458b656d3
David <David@David-PC>
2013-11-08 16:58:06 +0100

    PlayerTest 100% succesfull

---
+++ PerangKolom/test/game_StructureElements/PlayerTest.java
@@ -0,0 +1,33 @@
+package game_StructureElements;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class PlayerTest {
+
+	Player player1;
+	
+	@Before 
+	public void setUp() {
+		player1 = new Player("Bobo", 2);
+	}
+	
+	@Test
+	public void testPlayer() {
+		assertEquals("Bobo", player1.getName());
+		assertEquals(2, player1.getNumberOfPlayers());
+	}
+
+	@Test
+	public void testGetName() {
+		assertEquals("Bobo", player1.getName());
+	}
+
+	@Test
+	public void testGetNumberOfPlayers() {
+		assertEquals(2, player1.getNumberOfPlayers());
+	}
+
+}
