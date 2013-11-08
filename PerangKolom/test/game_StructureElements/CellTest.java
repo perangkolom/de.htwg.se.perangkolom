903ea7bc220c765482a2179eb0799687c3be4830
David <David@David-PC>
2013-11-08 12:35:08 +0100

    Test 1005 successful

--- PerangKolom/test/game_StructureElements/CellTest.java
+++ PerangKolom/test/game_StructureElements/CellTest.java
@@ -2,14 +2,29 @@ package game_StructureElements;
 
 import static org.junit.Assert.*;
 
+import org.junit.Before;
 import org.junit.Test;
 
 public class CellTest {
-
+	
+	Cell cell1;
+	
+	@Before 
+	public void setUp() {
+		cell1 = new Cell(10, 20, 30);
+	}
+	
+	//method for creating new Border object
+	public Border CellBorderMethod(boolean a, boolean b){
+		Border bord = new Border(a, b);
+		return bord;
+	}
+	
 	@Test
 	public void testCell() {
-		
-		assertEquals(8, new Cell(8, 0, 0));
+		assertEquals(1, new Cell(1, 2, 3).getPositionX());
+		assertEquals(2, new Cell(1, 2, 3).getPositionY());
+		assertEquals(3, new Cell(1, 2, 3).getCellValue());
 	}
  
 	@Test
@@ -21,73 +36,101 @@ public class CellTest {
 
 	@Test
 	public void testGetPositionY() {
-		assertEquals(5, new Cell(5, 0, 0).getPositionY());
-		assertEquals(7, new Cell(7, 0, 0).getPositionY());
+		assertEquals(5, new Cell(0, 5, 0).getPositionY());
+		assertEquals(7, new Cell(0, 7, 0).getPositionY());
 	}
 
 	@Test
 	public void testIsCellFilled() {
 		assertFalse(new Cell(5, 7, 0).isCellFilled());
+		assertFalse(new Cell(78, 33, 55).isCellFilled());
 	}
 
 	@Test
 	public void testSetCellFilled() {
-
+		cell1.setCellFilled(true);
+		assertTrue(cell1.isCellFilled());
 	}
 
 	@Test
 	public void testGetCellValue() {
-		fail("Not yet implemented");
+		assertEquals(5, new Cell (11,22,5).getCellValue());
 	}
 
 	@Test
 	public void testGetCellOwner() {
-		fail("Not yet implemented");
+		cell1.setCellOwner("Bobo");
+		assertEquals("Bobo", cell1.getCellOwner());
 	}
 
 	@Test
 	public void testSetCellOwner() {
-		fail("Not yet implemented");
+		cell1.setCellOwner("Bobo");
+		assertEquals("Bobo" , cell1.getCellOwner());
 	}
 
 	@Test
 	public void testGetCellBorder_top() {
-		fail("Not yet implemented");
+		Border a = CellBorderMethod(true, true);
+		cell1.setCellBorder_top(a);
+		assertNotNull(a);
+		assertEquals(a, cell1.getCellBorder_top());
 	}
 
 	@Test
 	public void testSetCellBorder_top() {
-		fail("Not yet implemented");
+		Border a = CellBorderMethod(true, true);
+		cell1.setCellBorder_top(a);
+		assertNotNull(a);
+		assertEquals(a, cell1.getCellBorder_top());
 	}
 
 	@Test
 	public void testGetCellBorder_bottom() {
-		fail("Not yet implemented");
+		Border a = CellBorderMethod(true, true);
+		cell1.setCellBorder_bottom(a);
+		assertNotNull(a);
+		assertEquals(a, cell1.getCellBorder_bottom());
 	}
 
 	@Test
 	public void testSetCellBorder_bottom() {
-		fail("Not yet implemented");
+		Border a = CellBorderMethod(true, true);
+		cell1.setCellBorder_bottom(a);
+		assertNotNull(a);
+		assertEquals(a, cell1.getCellBorder_bottom());
 	}
 
 	@Test
 	public void testGetCellBorder_left() {
-		fail("Not yet implemented");
+		Border a = CellBorderMethod(true, true);
+		cell1.setCellBorder_left(a);
+		assertNotNull(a);
+		assertEquals(a, cell1.getCellBorder_left());
 	}
 
 	@Test
 	public void testSetCellBorder_left() {
-		fail("Not yet implemented");
+		Border a = CellBorderMethod(true, true);
+		cell1.setCellBorder_left(a);
+		assertNotNull(a);
+		assertEquals(a, cell1.getCellBorder_left());
 	}
 
 	@Test
 	public void testGetCellBorder_right() {
-		fail("Not yet implemented");
+		Border a = CellBorderMethod(true, true);
+		cell1.setCellBorder_right(a);
+		assertNotNull(a);
+		assertEquals(a, cell1.getCellBorder_right());
 	}
 
 	@Test
 	public void testSetCellBorder_right() {
-		fail("Not yet implemented");
+		Border a = CellBorderMethod(true, true);
+		cell1.setCellBorder_right(a);
+		assertNotNull(a);
+		assertEquals(a, cell1.getCellBorder_right());
 	}
 
 }
