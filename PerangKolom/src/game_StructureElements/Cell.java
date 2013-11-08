76a69996b62c2c1a064c0069e55775811e0e2119
chstegma <chstegma@3DLAB3.in.fhkn.de>
2013-11-08 15:52:16 +0100

    test1

--- PerangKolom/src/game_StructureElements/Cell.java
+++ PerangKolom/src/game_StructureElements/Cell.java
@@ -11,14 +11,14 @@ public class Cell {
 	private Border cellBorder_left;
 	private Border cellBorder_right;
 	
-	
+	public boolean schrott;
+
 	public Cell(final int positionX, final int positionY, final int cellValue) {
 		this.positionX = positionX;
 		this.positionY = positionY;
 		this.cellValue = cellValue;
 	}
-	
-	
+
 	public int getPositionX() {
 		return positionX;
 	}
@@ -30,9 +30,11 @@ public class Cell {
 	public boolean isCellFilled() {
 		return cellFilled;
 	}
+
 	public void setCellFilled(boolean cellFilled) {
 		this.cellFilled = cellFilled;
 	}
+
 	public int getCellValue() {
 		return cellValue;
 	}
@@ -40,34 +42,41 @@ public class Cell {
 	public String getCellOwner() {
 		return cellOwner;
 	}
+
 	public void setCellOwner(String cellOwner) {
 		this.cellOwner = cellOwner;
 	}
+
 	public Border getCellBorder_top() {
 		return cellBorder_top;
 	}
+
 	public void setCellBorder_top(Border cellBorder_top) {
 		this.cellBorder_top = cellBorder_top;
 	}
+
 	public Border getCellBorder_bottom() {
 		return cellBorder_bottom;
 	}
+
 	public void setCellBorder_bottom(Border cellBorder_bottom) {
 		this.cellBorder_bottom = cellBorder_bottom;
 	}
+
 	public Border getCellBorder_left() {
 		return cellBorder_left;
 	}
+
 	public void setCellBorder_left(Border cellBorder_left) {
 		this.cellBorder_left = cellBorder_left;
 	}
+
 	public Border getCellBorder_right() {
 		return cellBorder_right;
 	}
+
 	public void setCellBorder_right(Border cellBorder_right) {
 		this.cellBorder_right = cellBorder_right;
 	}
 
-	
-	
 }
