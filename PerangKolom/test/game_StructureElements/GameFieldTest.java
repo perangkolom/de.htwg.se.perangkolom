978c9e6aacfcae7bcaa9826bca4bb5b423f75fd0
David <David@David-PC>
2013-11-08 10:22:19 +0100

    Commit with Java Verson 1.6

---
+++ PerangKolom/test/game_StructureElements/GameFieldTest.java
@@ -0,0 +1,14 @@
+package game_StructureElements;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class GameFieldTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
