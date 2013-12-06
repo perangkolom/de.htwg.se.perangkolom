34df79f7c7c9edfbac8da78e594326fdcd541224
David <David@David-PC>
2013-12-06 09:39:36 +0100

    5 packages mit jre 1.7

--- PerangKolom/src/de/htwg/se/PerangKolom/StructureElements/Schrott.java
+++
@@ -1,5 +0,0 @@
-package de.htwg.se.PerangKolom.StructureElements;
-
-public class Schrott {
-int bumm; 
-}
--- PerangKolom/src/de/htwg/se/PerangKolom/aview/tui/TextUI.java
+++ PerangKolom/src/de/htwg/se/PerangKolom/aview/tui/TextUI.java
@@ -1,5 +1,34 @@
 package de.htwg.se.PerangKolom.aview.tui;
 
-public class TextUI {
+import de.htwg.se.PerangKolom.util.observer.Event;
+import de.htwg.se.PerangKolom.util.observer.IObservable;
+
+public class TextUI  implements IObservable  {
+
+	@Override
+	public void update(Event e) {
+		printTUI();
+	}
+	
+	
+	public boolean processInputLine(String line) {
+		boolean continu = true;
+		switch (line) { 
+		case "q" : 
+			continu = false;
+			break;
+		case "r" :
+			//controller.reset();
+			break;
+		case "n" : 
+			//controller.create();
+			break;
+		}
+			return continu;
+		}
+		
+	public static void printTUI() {
+		System.out.println("TUI of PerangKolom:");
+	}
 
 }
