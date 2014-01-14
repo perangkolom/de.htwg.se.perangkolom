7abd83e0f447cae4c3b498826ea1856b4a896b6c
David <David@David-PC>
2014-01-14 17:08:27 +0100

    push and commit with all abstract classes

--- PerangKolom/src/de/htwg/se/PerangKolom/model/impl/Border.java
+++ PerangKolom/src/de/htwg/se/PerangKolom/model/impl/Border.java
@@ -1,31 +1,16 @@
 package de.htwg.se.PerangKolom.model.impl;
 
-import de.htwg.se.PerangKolom.model.IBorder;
+import de.htwg.se.PerangKolom.model.AbstractBorder;
 
-public class Border implements IBorder{
 
-	private boolean isBorderFilled = false;
-	private boolean isOuterBorder = false;
-	
-	
+public class Border extends AbstractBorder{
+
 	public Border(boolean isBorderFilled, boolean isOuterBorder) {
 		this.isBorderFilled = isBorderFilled;
 		this.isOuterBorder = isOuterBorder;
 	}
+
+
 	
-	public boolean isBorderFilled() {
-		return isBorderFilled;
-	}
-	
-	public void setBorderFilled(boolean isBorderFilled) {
-		this.isBorderFilled = isBorderFilled;
-	}
-	
-	public boolean isOuterBorder() {
-		return isOuterBorder;
-	}
-	public void setOuterBorder(boolean isOuterBorder) {
-		this.isOuterBorder = isOuterBorder;
-	}
 
 }
