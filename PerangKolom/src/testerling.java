a0e85d116ed2a0c5073dfb64aa2094bbce1bb2d5
David <David@David-PC>
2014-01-16 01:14:45 +0100

    commit with chris files 2

---
+++ PerangKolom/src/testerling.java
@@ -0,0 +1,15 @@
+import de.htwg.se.PerangKolom.model.impl.Cell;
+import de.htwg.se.PerangKolom.model.impl.CellArray;
+
+
+public final class testerling {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Cell[][] c = CellArray.getInstance();
+		System.out.println(c[0][0].getCellBorder_top());
+		
+	}
+
+}
